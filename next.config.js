043ccd70c0c0613b5596b3e06475bd346a9a3120
yuiseki <yuiseki@gmail.com>
2021-12-18 10:48:35 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: "/tokyo-opendata-dashboard",
+
+  webpack(config) {
+    config.resolve.alias["mapbox-gl"] = "maplibre-gl";
+    return config;
+  },
+};
