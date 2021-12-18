780cb13a2440da96bf5fc6f191ef70ac94bf3172
silloi <silloi1993@gmail.com>
2021-12-18 16:36:15 +0900

    chore: delete unused json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useState, useCallback } from "react";
 import MapGL, { Marker } from "react-map-gl";
-import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from "react-minimal-pie-chart";
 
 const geojson = {
   type: 'FeatureCollection',
@@ -31,24 +31,6 @@ const geojson = {
     }
   ]
 };
-
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
-];
-
-const data02 = [
-  { name: 'Group A', value: 2400 },
-  { name: 'Group B', value: 4567 },
-  { name: 'Group C', value: 1398 },
-  { name: 'Group D', value: 9800 },
-  { name: 'Group E', value: 3908 },
-  { name: 'Group F', value: 4800 },
-];
 
 const CustomPieChart = () => {
   return (
